b415821265b1cc1a4fc45ae2476bc0452cf6b66b
can <68462318+canca-n@users.noreply.github.com>
2022-08-03 08:41:57 +0900

    add function

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,11 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるTODOは5個まで！消化して！</p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
